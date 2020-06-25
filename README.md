920d8bc72b4e8796fd538c21a5f8a3e170b378bb
Vitaly Melnikov <vmelnikov@bk.ru>
2020-06-25 13:22:16 +0300

    Step 3: Added funcionality - JSON parser

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Difference calculator
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/a99a88d28ad37a79dbf6/maintainability)](https://codeclimate.com/github/Vmelnikoff/frontend-project-lvl2/maintainability)
+![Node.js CI](https://github.com/Vmelnikoff/frontend-project-lvl2/workflows/Node.js%20CI/badge.svg)
--- src/index.js
+++ src/index.js
@@ -1,4 +1,45 @@
+// CLI Builder for node.js command-line interfaces - https://github.com/tj/commander.js/
 import commander from 'commander';
+// Lodash utility library - https://lodash.com/
+import _ from 'lodash';
+// File System Node Module - https://nodejs.org/api/fs.html
+import fs from 'fs';
+// Path Node Module - https://nodejs.org/api/path.html
+import path from 'path';
+
+const parseJsonObj = ([jsonObj1, jsonObj2]) => {
+  const keys = _.union(Object.keys(jsonObj1), Object.keys(jsonObj2));
+  const result = keys.map((key) => {
+    const [value1, value2] = [jsonObj1[key], jsonObj2[key]];
+
+    if (_.has(jsonObj1, key) && _.has(jsonObj2, key)) {
+      return (value1 === value2)
+        ? `    ${key}: ${jsonObj1[key]}\n`
+        : `  - ${key}: ${jsonObj1[key]}\n  + ${key}: ${jsonObj2[key]}\n`;
+    }
+
+    return (_.has(jsonObj1, key))
+      ? `  - ${key}: ${jsonObj1[key]}\n`
+      : `  + ${key}: ${jsonObj2[key]}\n`;
+  });
+
+  return ['\n{\n', ...result, '}'].join('');
+};
+
+const getObjsFromFiles = (filepath1, filepath2) => {
+  // Build absolute paths
+  const cwd = process.cwd();
+  const absoluteFilePath1 = path.resolve(cwd, filepath1);
+  const absoluteFilePath2 = path.resolve(cwd, filepath2);
+
+  // Read data from files
+  const file1 = fs.readFileSync(absoluteFilePath1, 'utf-8');
+  const file2 = fs.readFileSync(absoluteFilePath2, 'utf-8');
+  const jsonObj1 = JSON.parse(file1);
+  const jsonObj2 = JSON.parse(file2);
+
+  return [jsonObj1, jsonObj2];
+};
 
 const program = commander
   .version('1.0.0')
@@ -6,7 +47,9 @@ const program = commander
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    console.log(filepath1, filepath2)
+    const result = parseJsonObj(getObjsFromFiles(filepath1, filepath2));
+    console.log(result);
+    return result;
   })
   .parse(process.argv);
 
