e39f33108a95e913956ddc967dde60ea4bc95c8e
Vitaly Melnikov <vmelnikov@bk.ru>
2020-08-10 12:44:43 +0300

    Refactoring: Updated genDiff test - describe.each for formatters

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,37 +2,16 @@ import { test, expect, describe } from '@jest/globals';
 import genDiff from '../src/index';
 import { readFile } from '../src/parsers';
 
-describe('Generator difference in two files', () => {
-  const typeFiles = ['json', 'yml', 'ini'];
+describe.each(['stylish', 'plain', 'json'])('Generator difference in two files', (format) => {
   const fixturesDir = '__fixtures__/';
 
-  describe('Format - stylish:', () => {
-    test.each(typeFiles)('Load %s files and output differences', (ext) => {
+  describe(`Format - ${format}:`, () => {
+    test.each(['json', 'yml', 'ini'])('Load %s files and output differences', (ext) => {
       const filepath1 = `${fixturesDir}file1.${ext}`;
       const filepath2 = `${fixturesDir}file2.${ext}`;
-      const expectedResult = readFile(`${fixturesDir}stylishResult.txt`).trimRight();
+      const expectedResult = readFile(`${fixturesDir}${format}Result.txt`).trimRight();
 
-      expect(genDiff(filepath1, filepath2, 'stylish')).toEqual(expectedResult);
-    });
-  });
-
-  describe('Format - plain:', () => {
-    test.each(typeFiles)('Load %s files and output differences', (ext) => {
-      const filepath1 = `${fixturesDir}file1.${ext}`;
-      const filepath2 = `${fixturesDir}file2.${ext}`;
-      const expectedResult = readFile(`${fixturesDir}plainResult.txt`).trimRight();
-
-      expect(genDiff(filepath1, filepath2, 'plain')).toEqual(expectedResult);
-    });
-  });
-
-  describe('Format - json:', () => {
-    test.each(typeFiles)('Load %s files and output differences', (ext) => {
-      const filepath1 = `${fixturesDir}file1.${ext}`;
-      const filepath2 = `${fixturesDir}file2.${ext}`;
-      const expectedResult = readFile(`${fixturesDir}jsonResult.txt`).trimRight();
-
-      expect(genDiff(filepath1, filepath2, 'json')).toEqual(expectedResult);
+      expect(genDiff(filepath1, filepath2, format)).toEqual(expectedResult);
     });
   });
 });
