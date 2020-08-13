528ec2101b03fea392ed4ee634cf21819774b487
Vitaly Melnikov <vmelnikov@bk.ru>
2020-08-13 17:24:10 +0300

    Refactoring: Division of responsibility in the parser

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
-import parserFactory from './parsers.js';
+// File System Node Module - https://nodejs.org/api/fs.html
+import fs from 'fs';
+// Path Node Module - https://nodejs.org/api/path.html
+import path from 'path';
+import parse from './parsers.js';
+import formatOutput from './formaters/index.js';
 
-export default (filepath1, filepath2, format = 'stylish') => parserFactory(filepath1, filepath2, format);
+const getPath = (filename) => `${path.resolve(process.cwd(), filename)}`;
+const readFile = (filename) => fs.readFileSync(getPath(filename), 'utf-8');
+
+export default (filepath1, filepath2, format = 'stylish') => {
+  const oldContent = readFile(filepath1);
+  const newContent = readFile(filepath2);
+  const extension = path.extname(filepath1) && path.extname(filepath2);
+
+  const diff = parse(oldContent, newContent, extension);
+
+  return formatOutput(diff, format);
+};
--- src/parsers.js
+++ src/parsers.js
@@ -1,36 +1,26 @@
-// File System Node Module - https://nodejs.org/api/fs.html
-import fs from 'fs';
-// Path Node Module - https://nodejs.org/api/path.html
-import path from 'path';
 // JS-YAML parser - https://github.com/nodeca/js-yaml
 import yaml from 'js-yaml';
 // INI parser - https://github.com/npm/ini
 import ini from 'ini';
 
 import buildDiff from './buildDiff.js';
-import formatOutput from './formaters/index.js';
 
-const getPath = (filename) => `${path.resolve(process.cwd(), filename)}`;
-export const readFile = (filename) => fs.readFileSync(getPath(filename), 'utf-8');
-
-export default (filepath1, filepath2, format) => {
-  switch (path.extname(filepath1) && path.extname(filepath2)) {
+const getJsonObj = (content, extension) => {
+  switch (extension) {
     case '.json':
-      return formatOutput(buildDiff(
-        JSON.parse(readFile(filepath1)),
-        JSON.parse(readFile(filepath2)),
-      ), format);
+      return JSON.parse(content);
     case '.yml':
-      return formatOutput(buildDiff(
-        yaml.safeLoad(readFile(filepath1)),
-        yaml.safeLoad(readFile(filepath2)),
-      ), format);
+      return yaml.safeLoad(content);
     case '.ini':
-      return formatOutput(buildDiff(
-        ini.parse(readFile(filepath1)),
-        ini.parse(readFile(filepath2)),
-      ), format);
+      return ini.parse(content);
     default:
-      return 'No parser for this extension or different extension';
+      throw new Error(`No parser for this extension - ${extension.slice(1)}!`);
   }
+};
+
+export default (oldContent, newContent, extension) => {
+  const oldJsonObj = getJsonObj(oldContent, extension);
+  const newJsonObj = getJsonObj(newContent, extension);
+
+  return buildDiff(oldJsonObj, newJsonObj);
 };
